0dcc216d45776b24756cc5d2d264933b54864d80
Sean <770899447@qq.com>
2022-05-15 21:47:06 +0800

    删除无用文件

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   // build path
-  base: '/mangosteen/dist/',
+  // base: '/mangosteen/dist/',
   plugins: [vue()],
 });
