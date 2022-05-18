b9bfe80b80ec1e15e518566459dad7987535e8f5
Sean <770899447@qq.com>
2022-05-18 09:43:00 +0800

    useSwipe基本

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -1,17 +1,24 @@
-import { defineComponent, Transition, VNode } from 'vue';
+import { defineComponent, ref, Transition, VNode, watchEffect } from 'vue';
 import { RouteLocationNormalizedLoaded, RouterView, useRoute } from 'vue-router';
 import logoSvg from '/src/assets/icons/logo.svg';
 import styles from './index.module.scss';
+import { useSwipe } from '../../hooks/useSwipe';
 
 export const Welcome = defineComponent({
   setup: (props, context) => {
+    const main = ref<HTMLElement | null>(null);
+    const { direction } = useSwipe(main);
+    watchEffect(() => {
+      console.log(direction.value);
+    });
+
     return () => (
       <div class={styles.wrapper}>
         <header>
           <img src={logoSvg} />
           <h1>山竹记账</h1>
         </header>
-        <main class={styles.main}>
+        <main class={styles.main} ref={main}>
           <Transition
             enterFromClass={styles.enter_from}
             enterActiveClass={styles.enter_active}
