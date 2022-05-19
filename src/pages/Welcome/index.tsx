39d7a834c0885b80fce3d6ddf59567eceb0e6f18
Sean <770899447@qq.com>
2022-05-19 17:34:43 +0800

    修复了transi放在routerview外面的问题

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -1,4 +1,4 @@
-import { defineComponent, ref, Transition, watchEffect } from 'vue';
+import { defineComponent, ref, Transition, VNode, watchEffect } from 'vue';
 import { RouterView, useRoute, useRouter } from 'vue-router';
 import logoSvg from '/src/assets/icons/logo.svg';
 import styles from './index.module.scss';
@@ -29,14 +29,21 @@ export const Welcome = defineComponent({
           <h1>山竹记账</h1>
         </header>
         <main class={styles.main} ref={main}>
-          <Transition
-            enterFromClass={styles.enter_from}
-            enterActiveClass={styles.enter_active}
-            leaveActiveClass={styles.leave_active}
-            leaveToClass={styles.leave_to}
-          >
-            <RouterView name="main" key={useRoute()?.params?.id.toString()}></RouterView>
-          </Transition>
+          <RouterView name="main">
+            {({ Component: X }: { Component: VNode }) => {
+              return (
+                <Transition
+                  enterFromClass={styles.enter_from}
+                  enterActiveClass={styles.enter_active}
+                  leaveActiveClass={styles.leave_active}
+                  leaveToClass={styles.leave_to}
+                >
+                  {/* @ts-ignore */}
+                  <X key={useRoute()?.params?.id.toString()}></X>
+                </Transition>
+              );
+            }}
+          </RouterView>
         </main>
         <footer>
           <RouterView name="footer" key={useRoute()?.params?.id.toString()} />
