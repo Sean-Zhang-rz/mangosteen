796c7c85c32a9e6610f5f53184182aea2e9b1fd3
Sean <770899447@qq.com>
2022-05-19 10:45:30 +0800

    start页icon

---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,9 @@
+import { defineComponent } from 'vue';
+import styles from './index.module.scss';
+
+export const NavBar = defineComponent({
+  // props: {},
+  setup: (props, context) => {
+    return () => <div>Menu</div>;
+  },
+});
--- src/pages/StartPage/inedx.tsx
+++ src/pages/StartPage/inedx.tsx
@@ -1,7 +1,9 @@
 import { defineComponent } from 'vue';
 import { Button } from '@/components/Button';
-import styles from './index.module.scss';
 import { FloatButton } from '@/components/FloatButton';
+import { Icon } from '@/components/Icon';
+import { NavBar } from '@/components/NavBar';
+import styles from './index.module.scss';
 
 export const StartPage = defineComponent({
   setup: (props, context) => {
@@ -9,10 +11,16 @@ export const StartPage = defineComponent({
       console.log('hi');
     };
     return () => (
-      <div class={styles.btn_wrapper}>
-        <Button class={styles.btn} onClick={onClick}>
-          开始记账
-        </Button>
+      <div class={styles.container}>
+        <NavBar />
+        <div class={styles.icon_wrapper}>
+          <Icon name="pig" class={styles.icon} />
+        </div>
+        <div class={styles.btn_wrapper}>
+          <Button class={styles.btn} onClick={onClick}>
+            开始记账
+          </Button>
+        </div>
         <FloatButton name="add" />
       </div>
     );
