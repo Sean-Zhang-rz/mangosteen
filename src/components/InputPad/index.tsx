512d02c62744d7a02bf1480dc9b5cd40f84aa2bc
Sean <770899447@qq.com>
2022-05-21 16:45:48 +0800

    wip:开始封装inputnumber组件

--- src/components/InputPad/index.tsx
+++ src/components/InputPad/index.tsx
@@ -3,6 +3,13 @@ import styles from './index.module.scss';
 
 export const InputPad = defineComponent({
   setup: (props, context) => {
-    return () => <div>InputPad</div>;
+    return () => (
+      <div class={styles.number_keyboard}>
+        <div class={styles.number_keyboard__header}>header</div>
+        <div class={styles.number_keyboard__body}>
+          <div class={styles.number_keyboard__body_keys}></div>
+        </div>
+      </div>
+    );
   },
 });
