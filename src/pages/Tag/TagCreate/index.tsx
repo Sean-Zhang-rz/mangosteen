a6dda430503f84ce270d9c6aded98f7f062d85f8
Sean <770899447@qq.com>
2022-05-22 21:09:56 +0800

    封装emojiList组件

--- src/pages/Tag/TagCreate/index.tsx
+++ src/pages/Tag/TagCreate/index.tsx
@@ -1,9 +1,14 @@
+import { EmojiList } from '@/components/EmojiList';
 import { MainLayout } from '@/components/MainLayout';
-import { defineComponent } from 'vue';
+import { defineComponent, reactive } from 'vue';
 import styles from './index.module.scss';
 
 export const TagCreate = defineComponent({
   setup: (props, context) => {
+    const formData = reactive({
+      name: '',
+      sign: '',
+    });
     return () => (
       <MainLayout title="新建标签" icon="back">
         <form class={styles.form}>
@@ -11,7 +16,10 @@ export const TagCreate = defineComponent({
             <label class={styles.form_label}>
               <span class={styles.form_item_name}>标签名</span>
               <div class={styles.form_item_value}>
-                <input type="text" class={[styles.form_item, styles.input, styles.error]} />
+                <input
+                  v-model={formData.name}
+                  class={[styles.form_item, styles.input, styles.error]}
+                />
               </div>
               <div class={styles.form_item_errorHint}>
                 <span>必填</span>
@@ -22,28 +30,7 @@ export const TagCreate = defineComponent({
             <label class={styles.form_label}>
               <span class={styles.form_item_name}>符号</span>
               <div class={styles.form_item_value}>
-                <nav>
-                  <span>表情</span>
-                  <span>手势</span>
-                  <span>职业</span>
-                  <span>衣服</span>
-                  <span>动物</span>
-                  <span>自然</span>
-                  <span>食物</span>
-                  <span>运动</span>
-                </nav>
-                <ol>
-                  <li>1</li>
-                  <li>2</li>
-                  <li>3</li>
-                  <li>4</li>
-                  <li>5</li>
-                  <li>6</li>
-                  <li>7</li>
-                  <li>8</li>
-                  <li>9</li>
-                  <li>10</li>
-                </ol>
+                <EmojiList />
               </div>
               <div class={styles.form_item_errorHint}>
                 <span>必填</span>
