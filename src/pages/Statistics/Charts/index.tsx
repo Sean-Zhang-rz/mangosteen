7aec16914063ce5c9e657a23afc9fd5d1a195806
Sean <770899447@qq.com>
2022-05-28 09:20:40 +0800

    引入echarts，线图和饼图

--- src/pages/Statistics/Charts/index.tsx
+++ src/pages/Statistics/Charts/index.tsx
@@ -1,6 +1,6 @@
+import { defineComponent, PropType, onMounted, ref } from 'vue';
+import * as echarts from 'echarts';
 import { FormItem } from '@/components/Form/Components/FormItem';
-import { defineComponent, PropType, ref } from 'vue';
-
 import styles from './index.module.scss';
 
 export const Charts = defineComponent({
@@ -16,7 +16,55 @@ export const Charts = defineComponent({
   },
   setup: (props, context) => {
     const category = ref('expenses');
-
+    const refDiv = ref<HTMLDivElement>();
+    const refDiv2 = ref<HTMLDivElement>();
+    onMounted(() => {
+      if (refDiv.value === undefined || refDiv2.value === undefined) return;
+      // 基于准备好的dom，初始化echarts实例
+      const lineChart = echarts.init(refDiv.value);
+      const pieChart = echarts.init(refDiv2.value);
+      // 绘制图表
+      lineChart.setOption({
+        grid: [{ left: 0, top: 0, right: 0, bottom: 20 }],
+        xAxis: {
+          type: 'category',
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        },
+        yAxis: {
+          type: 'value',
+        },
+        series: [
+          {
+            data: [150, 230, 224, 218, 135, 147, 260],
+            type: 'line',
+          },
+        ],
+      });
+      pieChart.setOption({
+        grid: [{ left: 0, top: 0, right: 0, bottom: 20 }],
+        series: [
+          {
+            name: 'Access From',
+            type: 'pie',
+            radius: '50%',
+            data: [
+              { value: 1048, name: 'Search Engine' },
+              { value: 735, name: 'Direct' },
+              { value: 580, name: 'Email' },
+              { value: 484, name: 'Union Ads' },
+              { value: 300, name: 'Video Ads' },
+            ],
+            emphasis: {
+              itemStyle: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: 'rgba(0, 0, 0, 0.5)',
+              },
+            },
+          },
+        ],
+      });
+    });
     return () => (
       <div class={styles.wrapper}>
         <FormItem
@@ -28,6 +76,8 @@ export const Charts = defineComponent({
             { value: 'income', text: '收入' },
           ]}
         />
+        <div ref={refDiv} class={styles.demo}></div>
+        <div ref={refDiv2} class={styles.demo2}></div>
       </div>
     );
   },
