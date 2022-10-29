b1d928f1b2540f0fa8062410900cbf40c3475b38
Sean <770899447@qq.com>
2022-10-29 09:57:10 +0800

    封装Tags组件

---
+++ src/pages/Item/components/Create/Tags.tsx
@@ -0,0 +1,44 @@
+import { defineComponent } from 'vue';
+import { getTags } from '@/api/tags';
+import { Button } from '@/components/Button';
+import { Icon } from '@/components/Icon';
+import useTags from '@/hooks/useTags';
+import { onError } from '@/utils/onError';
+
+import styles from './index.module.scss';
+const Tags = defineComponent({
+  props: {
+    kind: {
+      type: String,
+      required: true,
+    },
+  },
+  setup: (props) => {
+    const { tags: tagList, hasMore } = useTags((p) =>
+      getTags({ kind: props.kind, page: p + 1 }).catch(onError)
+    );
+    return () => (
+      <>
+        <div class={styles.main}>
+          <div class={styles.tag}>
+            <div class={styles.sign}>
+              <Icon name="add" class={styles.createTag} />
+            </div>
+            <div class={styles.name}>新增</div>
+          </div>
+          {tagList.value.map((tag) => (
+            <div class={[styles.tag, styles.selected]}>
+              <div class={styles.sign}>{tag.sign}</div>
+              <div class={styles.name}>{tag.name}</div>
+            </div>
+          ))}
+        </div>
+        <div class={styles.loadMore}>
+          {hasMore.value ? <Button>加载更多</Button> : <span>没有更多了</span>}
+        </div>
+      </>
+    );
+  },
+});
+
+export default Tags;
--- src/pages/Item/components/Create/index.tsx
+++ src/pages/Item/components/Create/index.tsx
@@ -1,78 +1,29 @@
-import { defineComponent, onMounted, ref } from 'vue';
-import { InputPad } from '@/components/InputPad';
+import { defineComponent, ref } from 'vue';
 import { MainLayout } from '@/components/MainLayout';
 import { Tabs } from '@/components/Tabs';
 import { Tab } from '@/components/Tabs/Tab';
-import { Icon } from '@/components/Icon';
-import { Button } from '@/components/Button';
-import { TagDTO } from '@/api/types/tags';
-import { getTags } from '@/api/tags';
-import { onError } from '@/utils/onError';
-
+import { InputPad } from '@/components/InputPad';
+import Tags from './Tags';
 import styles from './index.module.scss';
-import useTags from '@/hooks/useTags';
 
 export const ItemCreate = defineComponent({
   setup: (props, context) => {
     const refKind = ref<'支出' | '收入'>('支出');
-    const { tags: expensesTagList, hasMore: expensesHasMore } = useTags((p) =>
-      getTags({ kind: 'expenses', page: p + 1 }).catch(onError)
-    );
-    const { tags: incomeTagList, hasMore: incomeHasMore } = useTags((p) =>
-      getTags({ kind: 'income', page: p + 1 }).catch(onError)
-    );
-
     return () => (
       <MainLayout title="记一笔" icon="back">
-        {{
-          default: () => (
-            <div class={styles.wrapper}>
-              <Tabs v-model:selected={refKind.value} class={styles.tabs}>
-                <Tab name="支出" class={styles.tags_wrapper}>
-                  <div class={styles.main}>
-                    <div class={styles.tag}>
-                      <div class={styles.sign}>
-                        <Icon name="add" class={styles.createTag} />
-                      </div>
-                      <div class={styles.name}>新增</div>
-                    </div>
-                    {expensesTagList.value.map((tag) => (
-                      <div class={[styles.tag, styles.selected]}>
-                        <div class={styles.sign}>{tag.sign}</div>
-                        <div class={styles.name}>{tag.name}</div>
-                      </div>
-                    ))}
-                  </div>
-                  <div class={styles.loadMore}>
-                    {expensesHasMore.value ? <Button>加载更多</Button> : <span>没有更多了</span>}
-                  </div>
-                </Tab>
-                <Tab name="收入" class={styles.tags_wrapper}>
-                  <div class={styles.main}>
-                    <div class={styles.tag}>
-                      <div class={styles.sign}>
-                        <Icon name="add" class={styles.createTag} />
-                      </div>
-                      <div class={styles.name}>新增</div>
-                    </div>
-                    {incomeTagList.value.map((tag) => (
-                      <div class={[styles.tag, styles.selected]}>
-                        <div class={styles.sign}>{tag.sign}</div>
-                        <div class={styles.name}>{tag.name}</div>
-                      </div>
-                    ))}
-                  </div>
-                  <div class={styles.loadMore}>
-                    {incomeHasMore.value ? <Button>加载更多</Button> : <span>没有更多了</span>}
-                  </div>
-                </Tab>
-              </Tabs>
-              <div class={styles.inputPad_wrapper}>
-                <InputPad />
-              </div>
-            </div>
-          ),
-        }}
+        <div class={styles.wrapper}>
+          <Tabs v-model:selected={refKind.value} class={styles.tabs}>
+            <Tab name="支出" class={styles.tags_wrapper}>
+              <Tags kind="expenses" key="expenses" />
+            </Tab>
+            <Tab name="收入" class={styles.tags_wrapper}>
+              <Tags kind="income" key="income" />
+            </Tab>
+          </Tabs>
+          <div class={styles.inputPad_wrapper}>
+            <InputPad />
+          </div>
+        </div>
       </MainLayout>
     );
   },
