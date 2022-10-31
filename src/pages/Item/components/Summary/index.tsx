ae12472020204eb0f2d057a028ae4635d87c05b9
Sean <770899447@qq.com>
2022-10-31 21:47:57 +0800

    封装datetime

--- src/pages/Item/components/Summary/index.tsx
+++ src/pages/Item/components/Summary/index.tsx
@@ -5,6 +5,7 @@ import { ItemDTO } from '@/api/types/items';
 import { getItems } from '@/api/item';
 import { onError } from '@/utils/onError';
 import { Button } from '@/components/Button';
+import { DateTime } from '@/pages/Components/Datetime';
 
 export const ItemSummary = defineComponent({
   props: {
@@ -18,21 +19,23 @@ export const ItemSummary = defineComponent({
     },
   },
   setup: (props, context) => {
-    const itemList = ref<ItemDTO[]>([])
-    const hasMore = ref(false)
-    const page = ref(0)
+    const itemList = ref<ItemDTO[]>([]);
+    const hasMore = ref(false);
+    const page = ref(0);
     const fetchItems = async () => {
-      const { data: { itemsList: items, pager } } = await getItems({
+      const {
+        data: { itemsList: items, pager },
+      } = await getItems({
         happen_at: props.startDate,
         happen_before: props.endDate,
-        page: page.value + 1
-      }).catch(onError)
+        page: page.value + 1,
+      }).catch(onError);
       itemList.value.push(...items);
-      hasMore.value = (pager.page - 1) * pager.per_page + items.length < pager.count
-      page.value += 1
-    }
+      hasMore.value = (pager.page - 1) * pager.per_page + items.length < pager.count;
+      page.value += 1;
+    };
 
-    onMounted(fetchItems)
+    onMounted(fetchItems);
 
     return () => (
       <div class={styles.wrapper}>
@@ -50,33 +53,39 @@ export const ItemSummary = defineComponent({
             <span>39</span>
           </li>
         </ul>
-        {
-          itemList.value.length ? <>
+        {itemList.value.length ? (
+          <>
             <ol class={styles.list}>
-              {
-                itemList.value.map(item => <li>
+              {itemList.value.map((item) => (
+                <li>
                   <div class={styles.sign}>
                     <span>{item.tags_id[0]}</span>
                   </div>
                   <div class={styles.text}>
                     <div class={styles.tagAndAmount}>
                       <span class={styles.tag}>{item.tags_id[0]}</span>
-                      <span class={styles.amount}>￥<>{item.amount}</></span>
+                      <span class={styles.amount}>
+                        ￥<>{item.amount}</>
+                      </span>
                     </div>
-                    <div class={styles.time}>{item.happen_at}</div>
+                    <div class={styles.time}>
+                      <DateTime value={item.happen_at} />
+                    </div>
                   </div>
-                </li>)
-              }
+                </li>
+              ))}
             </ol>
             <div class={styles.more}>
-              {
-                hasMore.value
-                  ? <Button onClick={fetchItems}>向下滑动加载更多</Button>
-                  : <span>没有更多了</span>
-              }
+              {hasMore.value ? (
+                <Button onClick={fetchItems}>向下滑动加载更多</Button>
+              ) : (
+                <span>没有更多了</span>
+              )}
             </div>
-          </> : <div>记录为空</div>
-        }
+          </>
+        ) : (
+          <div>记录为空</div>
+        )}
         <FloatButton name="add" />
       </div>
     );
